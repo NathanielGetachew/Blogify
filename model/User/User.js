d7920934400cf38dbf04cac1bc127331e756e62e
NathanielGetachew <nathigechmaranata@gmail.com>
2023-08-25 14:09:38 +0300

    user model

---
+++ model/User/User.js
@@ -0,0 +1,93 @@
+const mongoose = require("mongoose");
+
+// schema
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    username: {
+      type: String,
+      require: true,
+    },
+    role: {
+      type: String,
+      require: true,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    lastLogin: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      default: false,
+    },
+    accountLevel: {
+      type: String,
+      enum: ["bronze", "silver", "gold"],
+      default: "bronze",
+    },
+
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+
+    coverImage: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    notificationPreferences: {
+      email: { type: String, default: true },
+      // other notification types, sms ...
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+    },
+    profileViewers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    blockedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    Posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+    likedPosts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+    passwordResetToken: {
+      type: String,
+    },
+    passwordRestExpires: {
+      type: Date,
+    },
+    accountVerificationToken: {
+      type: String,
+    },
+    accountVerificationExpires: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// compile the model
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
